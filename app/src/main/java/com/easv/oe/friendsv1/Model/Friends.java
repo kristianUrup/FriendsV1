e83eb4c825b6cfdd2a6eebebdff9c8de8cb0310d
oe <oe@easv.dk>
2019-02-19 11:11:08 +0100

    async removed

--- app/src/main/java/com/easv/oe/friendsv1/Model/Friends.java
+++ app/src/main/java/com/easv/oe/friendsv1/Model/Friends.java
@@ -9,22 +9,33 @@ public class Friends {
 	public Friends()
 	{
 		m_friends = new ArrayList<BEFriend>();
-		m_friends.add(new BEFriend("Ronni", "123456", true));
-		m_friends.add(new BEFriend("Kristjan", "234567"));
-		m_friends.add(new BEFriend("Nikolaj", "126256", true));
-		m_friends.add(new BEFriend("Jan", "234567"));
-		m_friends.add(new BEFriend("Hardy", "123456"));
-		m_friends.add(new BEFriend("Rasmus", "994567"));
-		m_friends.add(new BEFriend("Kenneth", "127426"));
-		m_friends.add(new BEFriend("Frederik", "204587", true));
-		m_friends.add(new BEFriend("Rune", "123456"));
-		m_friends.add(new BEFriend("Gleen", "234567", true));
-		m_friends.add(new BEFriend("Jimmy", "123456"));
-		m_friends.add(new BEFriend("Klaus", "234567"));
-		m_friends.add(new BEFriend("Jonathan", "123456"));
-		m_friends.add(new BEFriend("Mads", "234567", true));
-		m_friends.add(new BEFriend("Max", "123456"));
-		m_friends.add(new BEFriend("Andy", "234567"));
+		m_friends.add(new BEFriend("Alex", "123456", true));
+		m_friends.add(new BEFriend("Anni", "234567"));
+		m_friends.add(new BEFriend("Armandas", "126256", true));
+		m_friends.add(new BEFriend("Asger", "234567"));
+		m_friends.add(new BEFriend("Attila", "123456"));
+		m_friends.add(new BEFriend("Bastian", "994567"));
+		m_friends.add(new BEFriend("Bence", "127426"));
+		m_friends.add(new BEFriend("Bo", "204587", true));
+		m_friends.add(new BEFriend("Daniel", "123456"));
+		m_friends.add(new BEFriend("David", "234567", true));
+		m_friends.add(new BEFriend("Dominik", "123456"));
+		m_friends.add(new BEFriend("Jonas", "234567"));
+		m_friends.add(new BEFriend("Emil", "123456"));
+		m_friends.add(new BEFriend("Fabio", "234567", true));
+		m_friends.add(new BEFriend("Frederik", "123456"));
+		m_friends.add(new BEFriend("Jacob", "234567"));
+		m_friends.add(new BEFriend("Jan", "123456"));
+		m_friends.add(new BEFriend("Jesper", "234567"));
+		m_friends.add(new BEFriend("Kasper", "123456"));
+		m_friends.add(new BEFriend("kenneth", "234567", true));
+		m_friends.add(new BEFriend("Mads", "123456"));
+		m_friends.add(new BEFriend("Marius", "234567"));
+		m_friends.add(new BEFriend("Edwin", "234567"));
+		m_friends.add(new BEFriend("Michal", "123456"));
+		m_friends.add(new BEFriend("Morten", "234567", true));
+		m_friends.add(new BEFriend("Nicolai", "123456"));
+		m_friends.add(new BEFriend("Oliver", "234567"));
 	}
 	
 	public ArrayList<BEFriend> getAll()
@@ -33,8 +44,9 @@ public class Friends {
 	public String[] getNames()
 	{
 		String[] res = new String[m_friends.size()];
-		for(int idx=0; idx < res.length; idx++)
-			res[idx] = m_friends.get(idx).getName();
+		for (int i=0; i < res.length; i++) {
+			res[i] = m_friends.get(i).getName();
+		}
 		return res;
 	}
 	
